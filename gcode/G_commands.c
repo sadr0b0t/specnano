d564e36b85a33afce39e28d97d70bc65ea882744
Alexandr <alexandr@LapTop-Lin.(none)>
2013-01-08 18:36:56 +0400

    Added makefile, added extended G-command output and rebuilded libraries

--- gcode/G_commands.c
+++ gcode/G_commands.c
@@ -2,10 +2,10 @@
 
 extern void gcode_g01(double x, double y, double z, double f)
 {
-	printf("G1: %f %f %f\n", x, y, z);
+	printf("G1: X%f Y%f Z%f F%f\n", x, y, z, f);
 }
 
 extern void gcode_g02(double x, double y, double z, double f, double r)
 {
-	printf("G2: %f %f %f\n", x, y, z);
+	printf("G2: X%f Y%f Z%f R%f F%f\n", x, y, z, r, f);
 }
--- gcode/file_processing_method.c
+++ gcode/file_processing_method.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 int file_processing(FILE* f)
 {
 	int c=0;
